ebd07d5095c762d008c7a7606a851daddf192c51
dsambugaro <daniloc@alunos.utfpr.edu.br>
2019-06-26 20:17:16 -0300

    Iniciando frontend

--- passwdpp/src/App.js
+++ passwdpp/src/App.js
@@ -1,38 +1,18 @@
-import React, { Component } from "react";
-import logo from "./logo.svg";
-import "./App.css";
-import {setup_models} from './setup_db'
+import React, { Component } from 'react'
+import { Admin, Resource, ListGuesser } from 'react-admin'
 
-class App extends Component {
-  state={
+import { dataProvider } from './Utils'
+import { Dashboard } from './components/Dashboard'
+import { ListUsers } from './components/User'
 
-  }
-
-  constructor(props) {
-    super(props)
-    setup_models()
-  }
-
+export class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
-    );
+      <Admin dashboard={Dashboard} dataProvider={dataProvider}>
+        <Resource name="users" list={ListUsers} />
+      </Admin>
+    )
   }
 }
 
-export default App;
+export default App
