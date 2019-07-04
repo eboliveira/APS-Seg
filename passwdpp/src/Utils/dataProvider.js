0eddf0f6cfa81258cf9f7a19fddd5f4679346dd0
dsambugaro <daniloc@alunos.utfpr.edu.br>
2019-07-04 19:54:17 -0300

    Parser da API

---
+++ passwdpp/src/Utils/dataProvider.js
@@ -0,0 +1,137 @@
+import { stringify } from 'query-string';
+import {
+    GET_LIST,
+    GET_ONE,
+    CREATE,
+    UPDATE,
+    UPDATE_MANY,
+    DELETE,
+    DELETE_MANY,
+    GET_MANY,
+    GET_MANY_REFERENCE,
+} from 'react-admin';
+
+// const apiUrl = 'http://localhost:8080/'
+const apiUrl = 'http://jsonplaceholder.typicode.com'
+
+/**
+ * Maps react-admin queries to API
+ *
+ * @param {string} type Request type, e.g GET_LIST
+ * @param {string} resource Resource name, e.g. "posts"
+ * @param {Object} payload Request parameters. Depends on the request type
+ * @returns {Promise} the Promise for a data response
+ */
+export function dataProvider(type, resource, params) {
+    let url = ''
+    const options = {
+        headers: new Headers({
+            Accept: 'application/json',
+        }),
+    }
+
+    var query = null
+    switch (type) {
+        case GET_LIST:
+            {
+                const { page, perPage } = params.pagination
+                const { field, order } = params.sort
+                query = {
+                    sort: JSON.stringify([field, order]),
+                    range: JSON.stringify([
+                        (page - 1) * perPage,
+                        page * perPage - 1,
+                    ]),
+                    filter: JSON.stringify(params.filter),
+                };
+                url = `${apiUrl}/${resource}?${stringify(query)}`
+                // url = `${apiUrl}/${resource}`
+                break
+            }
+        case GET_ONE:
+            url = `${apiUrl}/${resource}/${params.id}`
+            break
+        case CREATE:
+            url = `${apiUrl}/${resource}`
+            options.method = 'POST'
+            options.body = JSON.stringify(params.data)
+            break
+        case UPDATE:
+            url = `${apiUrl}/${resource}/${params.id}`
+            options.method = 'PUT'
+            options.body = JSON.stringify(params.data)
+            break
+        case UPDATE_MANY:
+            query = {
+                filter: JSON.stringify({ id: params.ids }),
+            };
+            url = `${apiUrl}/${resource}?${stringify(query)}`
+            options.method = 'PATCH'
+            options.body = JSON.stringify(params.data)
+            break
+        case DELETE:
+            url = `${apiUrl}/${resource}/${params.id}`
+            options.method = 'DELETE'
+            break
+        case DELETE_MANY:
+            query = {
+                filter: JSON.stringify({ id: params.ids }),
+            };
+            url = `${apiUrl}/${resource}?${stringify(query)}`
+            options.method = 'DELETE'
+            break
+        case GET_MANY:
+            {
+                query = {
+                    filter: JSON.stringify({ id: params.ids }),
+                };
+                url = `${apiUrl}/${resource}?${stringify(query)}`
+                break
+            }
+        case GET_MANY_REFERENCE:
+            {
+                const { page, perPage } = params.pagination;
+                const { field, order } = params.sort;
+                query = {
+                    sort: JSON.stringify([field, order]),
+                    range: JSON.stringify([
+                        (page - 1) * perPage,
+                        page * perPage - 1,
+                    ]),
+                    filter: JSON.stringify({
+                        ...params.filter,
+                        [params.target]: params.id,
+                    }),
+                };
+                url = `${apiUrl}/${resource}?${stringify(query)}`
+                break;
+            }
+        default:
+            throw new Error(`Unsupported Data Provider request type ${type}`)
+    }
+
+    return fetch(url, options)
+        .then(res => res.json())
+        .then(json => {
+            console.log(json);
+            
+            switch (type) {
+                case GET_LIST:
+                    return { data: json, total: json.length }
+                // case GET_ONE:
+                //         return { data: json }
+                case CREATE:
+                    return { data: { ...params.data, id: json.id } }
+                // case UPDATE:
+                //     return { data: json }
+                // case DELETE:
+                //     return { data: json }
+                // case DELETE_MANY:
+                //     return { data: json }
+                default:
+                    return { data: json }
+            }
+        })
+}
+
+export default dataProvider
