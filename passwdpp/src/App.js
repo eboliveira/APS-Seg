18aac7317d86ff0378639192989e3cf800811b8f
dsambugaro <daniloc@alunos.utfpr.edu.br>
2019-07-04 19:53:56 -0300

    Base do frontend da aplicação

--- passwdpp/src/App.js
+++ passwdpp/src/App.js
@@ -1,15 +1,17 @@
 import React, { Component } from 'react'
-import { Admin, Resource, ListGuesser } from 'react-admin'
+import { Admin, Resource } from 'react-admin'
+import UserIcon from '@material-ui/icons/Group'
 
 import { dataProvider } from './Utils'
 import { Dashboard } from './components/Dashboard'
-import { ListUsers } from './components/User'
+import { ListUsers, EditUser } from './components/User'
+import { authProvider } from './components/authProvider'
 
 export class App extends Component {
   render() {
     return (
-      <Admin dashboard={Dashboard} dataProvider={dataProvider}>
-        <Resource name="users" list={ListUsers} />
+      <Admin dashboard={Dashboard} dataProvider={dataProvider} authProvider={authProvider}>
+        <Resource options={{ label: 'Usuários' }}name="users" icon={UserIcon} list={ListUsers} edit={EditUser} />
       </Admin>
     )
   }
