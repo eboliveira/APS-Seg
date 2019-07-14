2e73c4ac39427437ee04d1b4dd224db646953d0a
dsambugaro <daniloc@alunos.utfpr.edu.br>
2019-07-13 21:03:50 -0300

    rota para listar usuários

--- server/index.js
+++ server/index.js
@@ -17,84 +17,97 @@ const port = 8080;
 const PasswdSrv = new PasswordService();
 
 function now() {
-  return moment(new Date()).format('DD/MM/YYYY');
+    return moment(new Date()).format('DD/MM/YYYY');
 }
 
 app.use(bodyParser.json());
 
 app.listen(port, () => {
-  console.log(`Servidor rodando em http://localhost:${port}`);
+    console.log(`Servidor rodando em http://localhost:${port}`);
 });
 
 app.get("/", (req, res) => {
-  res.status(202).send("Server");
+    res.status(202).send("Server");
 });
 
 app.post("/pattern_passwd", (req, res) => {
-  passwd_pattern_controller.insert(req.body);
-  res.status(200).send("Received");
+    passwd_pattern_controller.insert(req.body);
+    res.status(200).send("Received");
 });
 
 app.get("/pattern_passwd", (req, res) => {
-  passwd_pattern_controller.getByUserId(req.body.user_id).then(resp => {
-    res.status(200).send(resp);
-  });
+    passwd_pattern_controller.get().then(resp => {
+        res.status(200).send(resp);
+    });
+});
+
+
+
+//listar usuários
+app.get("/user", (req, res) => {
+    const usersList = PasswdSrv.getUsers();
+
+    if (usersList) {
+        res.status(200).send(usersList);
+    } else {
+        res.status(500).send("Internal Server Error");
+    }
 });
 
 //criar usuário
 app.post("/user", (req, res) => {
-  const { username, password, user_id } = req.body;
-  const res_service = PasswdSrv.addUser(username, password);
-  const user = PasswdSrv.getUser(username);
- 
-  if (res_service && user_id != "undefined") {
-    logs_controller.insert(user_id, "create user", now(), user.id);
-    res.status(201).send(user);
-  } else {
-    res.status(400).send("Bad request");
-  }
+    const { username, password, user_id } = req.body;
+    const res_service = PasswdSrv.addUser(username, password);
+    const user = PasswdSrv.getUser(username);
+
+    if (res_service && user_id != "undefined") {
+        logs_controller.insert(user_id, "create user", now(), user.id);
+        res.status(201).send(user);
+    } else {
+        res.status(400).send("Bad request");
+    }
 });
 
 //alterar usuário
 app.put("/user", (req, res) => {
-  const { username, password, user_id, user_id_target } = req.body;
+    const { username, password, user_id, user_id_target } = req.body;
 
-  const res_service = PasswdSrv
-    .getUser(username);
-  
-  if (password) {
-    // TODO: Validar a força da senha
-    PasswdSrv.managerShadow.obj.password = 
-      Utils.generateHashPassword(password);
-    PasswdSrv.managerShadow.add();
-  }
+    const res_service = PasswdSrv
+        .getUser(username);
 
-  create_log(user_id, 'alter user', now());
+    if (password) {
+        // TODO: Validar a força da senha
+        PasswdSrv.managerShadow.obj.password =
+            Utils.generateHashPassword(password);
+        PasswdSrv.managerShadow.add();
+    }
 
-  logs_controller.insert(user_id,"alter user", now(),user_id_target);
+    create_log(user_id, 'alter user', now());
+
+    logs_controller.insert(user_id, "alter user", now(), user_id_target);
 });
 
 //deletar usuário
 app.delete("/user", (req, res) => {
-  const { user_id, user_id_target, username } = req.body;
-  const res_service = PasswdSrv.removeUser(username);
+    const { user_id, user_id_target, username } = req.body;
+    const res_service = PasswdSrv.removeUser(username);
 
-  if (res_service) {
-    logs_controller.insert(user_id,"delete user",now(),user_id_target);
-    res.status(201).send(user_id_target);
-  } else {
-    res.status(400).send("Bad request");
-  }
+    if (res_service) {
+        logs_controller.insert(user_id, "delete user", now(), user_id_target);
+        res.status(201).send(user_id_target);
+    } else {
+        res.status(400).send("Bad request");
+    }
 });
 
 //obter os registros de eventos
 app.get("/logs", (req, res) => {
-  logs_controller.get().then((resp) =>{
-    res.status(200).send(resp)
-  })
+    logs_controller.get().then((resp) => {
+        res.status(200).send(resp)
+    })
 });
 
 //verificar senha
 app.get("/check_passwd", (req, res) => {
-  
+
 });
